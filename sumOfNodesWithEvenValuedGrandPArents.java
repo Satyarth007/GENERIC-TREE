9cf6ccba3746d6324b8409ad2d08297f8efd61b6
SATYARTH <satyarthtiwari123@gmail.com>
2023-03-02 21:49:36 +0530

    LEETCODE 1315

---
+++ sumOfNodesWithEvenValuedGrandPArents.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumEvenGrandparent(TreeNode root) {
+        
+        int sum=0;
+        if(root==null) return sum;
+        if(root.val%2==0){
+            if(root.left!=null && root.left.left!=null) sum+=root.left.left.val;
+            if(root.left!=null && root.left.right!=null) sum+=root.left.right.val;
+            if(root.right!=null && root.right.left!=null) sum+=root.right.left.val;
+            if(root.right!=null && root.right.right!=null) sum+=root.right.right.val;
+            
+
+        }
+        if(root.left!=null)
+        sum+=sumEvenGrandparent(root.left);
+        if(root.right!=null)
+        sum+=sumEvenGrandparent(root.right);
+        return sum;
+        
+    }
+}
